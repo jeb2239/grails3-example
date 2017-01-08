65c34756263fa080fa52e76d5d0ee74aa366bad5
Jonathan Barrios <jeb2239@columbia.edu>
2017-01-08 17:42:44 -0500

    ahhahaha

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -10,4 +10,17 @@ environments {
       password = uri.userInfo.split(":")[1]
     }
   }
+
+  development {
+    dataSource {
+      dbCreate = "update"
+      driverClassName = "org.postgresql.Driver"
+      dialect = org.hibernate.dialect.PostgreSQLDialect
+      uri = new URI(System.env.DATABASE_URL?:"postgres://localhost:5432/test")
+      url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require"
+      username = uri.userInfo.split(":")[0]
+      password = uri.userInfo.split(":")[1]
+    }
+
+  }
 }
